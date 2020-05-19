36b3c6b9eeb610f3194fd82ffec4db08126e7616
Roaim Ahmed Hridoy <rah0024hr@gmail.com>
2020-05-19 13:59:25 +0600

    android: ui improvement

--- dtb-android/app/src/main/java/app/roaim/dtbazar/ui/store_details/FoodSuggestionAdapter.kt
+++ dtb-android/app/src/main/java/app/roaim/dtbazar/ui/store_details/FoodSuggestionAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import app.roaim.dtbazar.R
 import app.roaim.dtbazar.model.Food
 
 class FoodSuggestionAdapter(context: Context) :
@@ -12,13 +13,27 @@ class FoodSuggestionAdapter(context: Context) :
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = convertView ?: super.getView(position, convertView, parent)
-        (view as TextView).text = getItem(position)?.name
+        val item = getItem(position)
+        (view as TextView).text =
+            context.getString(
+                R.string.title_food_suggestion_spinner,
+                item?.name,
+                item?.currency,
+                item?.unit
+            )
         return view
     }
 
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = convertView ?: super.getView(position, convertView, parent)
-        (view as TextView).text = getItem(position)?.name
+        val item = getItem(position)
+        (view as TextView).text =
+            context.getString(
+                R.string.title_food_suggestion_spinner,
+                item?.name,
+                item?.currency,
+                item?.unit
+            )
         return view
     }
 }
