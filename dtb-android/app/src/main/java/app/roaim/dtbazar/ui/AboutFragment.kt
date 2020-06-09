3558221807e8118c3601b35caa304fa7fc9d7032
Roaim Ahmed Hridoy <rah0024hr@gmail.com>
2020-06-09 13:55:59 +0600

    android: fixes #33

--- dtb-android/app/src/main/java/app/roaim/dtbazar/ui/AboutFragment.kt
+++ dtb-android/app/src/main/java/app/roaim/dtbazar/ui/AboutFragment.kt
@@ -3,12 +3,15 @@ package app.roaim.dtbazar.ui
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import app.roaim.dtbazar.BuildConfig
 import app.roaim.dtbazar.databinding.FragmentAboutBinding
 import app.roaim.dtbazar.ui.store_details.AboutFragmentClickListener
 import app.roaim.dtbazar.utils.openInCustomTab
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
 
 
 class AboutFragment : Fragment(), AboutFragmentClickListener {
@@ -16,6 +19,7 @@ class AboutFragment : Fragment(), AboutFragmentClickListener {
     companion object {
         const val BASE_URL =
             "${BuildConfig.API_HOST}${BuildConfig.API_ENDPOINT_PREFIX}${BuildConfig.API_VERSION}"
+        private var ossOpenedAt = 0L
     }
 
     private var _binding: FragmentAboutBinding? = null
@@ -45,7 +49,25 @@ class AboutFragment : Fragment(), AboutFragmentClickListener {
     }
 
     override fun onOssLicencesClick() {
-        startActivity(Intent(requireContext(), OssLicensesMenuActivity::class.java))
+        /*
+         we don't want firebase test lab's bot to enter into a 3rd party Activity
+         (which we don't have control over) and crash our app due to that libraries memory leak
+        */
+        if (ossOpenedAt.plus(
+                Firebase.remoteConfig.getLong("oss_open_interval_second").times(1000)
+            ) > System.currentTimeMillis()
+        ) {
+            Toast.makeText(
+                requireContext(),
+                "Please try after sometime",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+        startActivity(Intent(requireContext(), OssLicensesMenuActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        })
+        ossOpenedAt = System.currentTimeMillis()
     }
 
     override fun onTermsClick() {
